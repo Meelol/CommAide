e4df049436c8225c56f83bc469eec07863da7140
Omar Rodriguez <eomarrodriguez@outlook.com>
2023-05-19 17:26:41 -0700

    Fixed images and sound

--- src/data/AideItems.js
+++ src/data/AideItems.js
@@ -1,33 +1,51 @@
 import AideItem from "./AideItemModel";
 
-const glassOfWater = new AideItem("water","Water",
-                "src/assets/Images/WaterGlass.jpg",
-                "src/assets/Audio/WaterGlass.mp3");
+import waterImg from "../assets/Images/WaterGlass.jpg";
+import waterSound from "../assets/Audio/WaterGlass.mp3"
 
-const restroom = new AideItem("restroom", "Go to the restroom",
-                            "src/assets/Images/Restroom.jpg",
-                            "src/assets/Audio/Restroom.mp3");
+import restroomImg from "../assets/Images/Restroom.jpg";
+import restroomSound from "../assets/Audio/Restroom.mp3";
 
-const thankYou = new AideItem("thank you", "Thank you",
-                            "src/assets/Images/Thankyou.png",
-                            "src/assets/Audio/Thankyou.mp3")
+import thankYouImg from "../assets/Images/ThankYou.jpg";
+import  thankYouSound from "../assets/Audio/ThankYou.mp3";
 
-const activityRoom = new AideItem("Activity Room", "Go to Activity Room",
-                            "src/assets/Images/ActivityRoom.jpg",
-                            "src/assets/Audio/ActivityRoom.mp3")
+import activityRoomImg from "../assets/Images/ActivityRoom.jpg";
+import activityRoomSound from "../assets/Audio/ActivityRoom.mp3"; 
+
+import backToRoomImg from "../assets/Images/Bedroom.jpg";
+import backToRoomSound from "../assets/Audio/Bedroom.mp3";
+
+import painImg from "../assets/Images/Pain.jpg";
+import painSound from "../assets/Audio/Pain.mp3";
+
+import foodImg from "../assets/Images/Food.jpg";
+import foodSound from "../assets/Audio/Food.mp3";
+
+import goOutsideImg from "../assets/Images/Outside.jpg";
+import goOutsideSound from "../assets/Audio/Outside.mp3";
+
+
+const glassOfWater = new AideItem("water","Water", 
+                         waterImg, waterSound);
+
+const restroom = new AideItem("restroom", "Go to the restroom", 
+                          restroomImg, restroomSound);
+
+const thankYou = new AideItem("thank you", "Thank you", 
+                          thankYouImg, thankYouSound);
+
+const activityRoom = new AideItem("Activity Room", "Go to Activity Room", 
+activityRoomImg, activityRoomSound);
+
 const backToRoom = new AideItem("Bedroom","Go to bedroom",
-                "src/assets/Images/Bedroom.jpg",
-                "src/assets/Audio/Bedroom.mp3");
-const pain = new AideItem("pain","I'm in pain",
-                "src/assets/Images/Pain.jpg",
-                "src/assets/Audio/Pain.mp3");
+backToRoomImg, backToRoomSound);
 
-const food = new AideItem("food","I'm hungry",
-                "src/assets/Images/Food.jpg",
-                "src/assets/Audio/Food.mp3");
-const goOutside = new AideItem("Outside","Go outside",
-                "src/assets/Images/Outside.jpg",
-                "src/assets/Audio/Outside.mp3");
+const pain = new AideItem("pain","I'm in pain", 
+painImg, painSound);
+
+const food = new AideItem("food","I'm hungry", foodImg, foodSound);
+
+const goOutside = new AideItem("Outside","Go outside", goOutsideImg, goOutsideSound);
                            
 const totalItems = [];
 
