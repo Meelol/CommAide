0118fad6830f0862e74fd9a8f1bb627daeb17824
Omar Rodriguez <eomarrodriguez@outlook.com>
2023-06-23 20:06:50 -0700

    Test

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,7 @@ function Welcome(){
         <React.Fragment>
         <h1 className="flex justify-center my-8 p-mb-4 text-4xl font-extrabold leading-none 
         tracking-tight text-gray-900 md:text-5xl lg:text-6xl 
-        dark:text-white">Welcome To CommAide</h1>
+        dark:text-white">Hola Samuel</h1>
         <p className="flex justify-center mb-6 text-lg font-normal text-gray-500 
         lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Your Voice Matters</p>
         </React.Fragment>
